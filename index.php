130b180a574dc43d7b07f401da0bfd4aa72c4536
Shawn <shawn@intersective.com>
2015-11-18 09:58:09 +1100

    initial

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Firepad-test</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" />
+	<script type="text/javascript" src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+</head>
+<body>
+	<center><h1>Welcome to Intersective Firepad-test</h1></center>
+<?php
+?>
+</body>
+</html>
